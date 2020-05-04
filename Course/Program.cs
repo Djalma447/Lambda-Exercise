417d53bc3702b998d770e74caa536d2ca0896dae
Djalma <djalma.ant@gmail.com>
2020-05-04 19:41:11 -0300

    My Personal Answer

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Course.Entities;
 
 namespace Course
 {
@@ -6,7 +9,17 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(p => p.Name[0] != 'T');
+            double sum = 0.0;
+            list.ForEach(p => sum += p.Price);
+            Console.WriteLine(sum.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
